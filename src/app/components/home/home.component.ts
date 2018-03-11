c811b7b8cb456588c2f909a7274d1f3eccad2bdd
jtmcandrew1 <jtmcandrew@live.com>
2018-03-11 09:27:50 -0500

    formatting player detail

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -39,8 +39,8 @@ export class HomeComponent implements OnInit {
   
       this._titleService.setTitle('Home | the golf pool');
 
-    this.years = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"];    
-    this.yearSelected = "2017";   
+    this.years = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"];    
+    this.yearSelected = "2018";   
 
     this.onYearChange();
 
